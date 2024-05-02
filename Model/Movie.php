9bd9180f792da2b33c830f1f57fab54e126e4802
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-02 14:52:37 +0200

    [add] struttura

---
+++ Model/Movie.php
@@ -0,0 +1,22 @@
+<?php
+class Movie {
+
+  public $titolo;
+  public $sottotitolo;
+  public $genere;
+
+
+  function __construct(string $_titolo, string $_sottotitolo, string $_genere ){
+
+    $this-> titolo = $_titolo;
+    $this-> sottotitolo = $_sottotitolo;
+    $this-> genere = $_genere;
+  }
+
+
+
+  public function
+}
+
+
+?>
